f5db397d7287838d4baa85d2ba8488f6e01d8576
zoe1105 <zoeline_2022@foxmail.com>
2026-02-05 14:23:50 +0800

    Delete api directory

--- api/index.py
+++
@@ -1,6 +0,0 @@
-from app import app  # 这里 app 是你 app.py 里 Flask(__name__) 创建的那个变量
-from flask import send_from_directory
-
-@app.route("/")
-def index():
-    return send_from_directory("public", "index.html")
